d1336a9fda9ae4b03bd42a326902a6ea554ef533
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-06 15:11:53 +0530

    New file

---
+++ Program11.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Program11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 10,sum=0;
+		for(int i = 1;i <= N ;i++)
+		{
+			sum = sum +i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
