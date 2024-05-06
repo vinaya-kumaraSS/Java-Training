316fcc3af5456eaae02044a61c06f45f8374e6ba
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-06 14:49:33 +0530

    New file

---
+++ Program7.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 40;
+		switch(a) {
+		case 10 -> System.out.println("number is 10");
+		case 20 -> System.out.println("number is 20");
+		case 30 -> System.out.println("number is 30");
+		default -> System.out.println("invalid input");
+		}
+	}
+
+}
