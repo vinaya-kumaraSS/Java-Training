6396c52c9ecb5b5a9d2eff831ce78ead3c5c3ed5
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-06 14:47:30 +0530

    New file

---
+++ Program3.java
@@ -0,0 +1,16 @@
+package Training;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		System.out.println("post increment :"+(a++));
+		System.out.println("pre increment :"+(++b));
+//		a = 10;
+//		b = 10;
+		System.out.println("post decrement :"+(a--));
+		System.out.println("pre decrement :"+(--b));
+	}
+
+}
