3bbf58f68e8cbea5d80832bac01db231c963fd7a
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-06 14:48:58 +0530

    New file

---
+++ Program6.java
@@ -0,0 +1,19 @@
+package Training;
+
+public class Program6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+//		if(a % 2 == 0)
+//			System.out.println("even");
+//		else
+//			System.out.println("odd");
+		
+		
+		
+//		using ternary operator
+		System.out.println((a % 2 == 0)?"even":"odd");
+	}
+
+}
