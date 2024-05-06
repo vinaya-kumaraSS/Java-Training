a004777c149fe854aeff20d968429047b011214c
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-06 14:48:25 +0530

    New file

---
+++ Program5.java
@@ -0,0 +1,12 @@
+package Training;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println("a<<1 :"+(a<<1));
+		System.out.println("a>>1 :"+(a>>1));
+	}
+
+}
