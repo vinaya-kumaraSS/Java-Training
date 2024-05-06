b18825febe6d018bba0081968dc009e541fd8f2c
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-06 14:47:01 +0530

    New file

---
+++ Program2.java
@@ -0,0 +1,16 @@
+package Training;
+public class Program2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Addition :"+c);
+		c = a - b;
+		System.out.println("Substraction :"+c);
+		c = a * b;
+		System.out.println("Multiplication :"+c);
+		c = a / b;
+		System.out.println("Division :"+c);
+}
+}
