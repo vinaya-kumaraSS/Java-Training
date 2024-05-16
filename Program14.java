241ba305501e0dde45a8c185cdbb84c5d721ff60
Vinayakumara S S <121751707+vinaya-kumaraSS@users.noreply.github.com>
2024-05-17 00:01:11 +0530

    New file

---
+++ Program14.java
@@ -0,0 +1,15 @@
+package Training;
+
+public class Program14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a1 = new int[10];
+		int[] a2 = {3,5,7,1,8,99,44,-10};
+		int[] a3 = {4,3,2,1};
+		System.out.println(a1.length);
+		System.out.println(a2.length);
+		System.out.println(a3.length);
+	}
+
+}
